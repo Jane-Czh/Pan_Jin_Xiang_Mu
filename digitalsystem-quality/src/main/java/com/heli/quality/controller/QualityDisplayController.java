2a8763da7189e8256fd61709ac736bc88f61e56a
loophong <1940155085@qq.com>
2024-05-27 11:32:37 +0800

    质量展示

--- digitalsystem-quality/src/main/java/com/heli/quality/controller/QualityDisplayController.java
+++ digitalsystem-quality/src/main/java/com/heli/quality/controller/QualityDisplayController.java
@@ -28,6 +28,51 @@ public class QualityDisplayController extends BaseController {
     private IQualityDisplayService qualityDisplayService;
 
     /**
+     * 三包期内新车返修率 指标57
+     */
+    @PostMapping("/warrantyRepairRate")
+    public TableDataInfo warrantyRepairRate(@RequestBody DisplayRequestParam time) {
+        List<DisplayEntity> list = qualityDisplayService.selectWarrantyRepairRate(time.getStartTime(), time.getEndTime());
+        return getDataTable(list);
+    }
+
+    /**
+     * 三包期内整车月度返修率 指标58
+     */
+    @PostMapping("/warrantyVehicleRepairRate")
+    public TableDataInfo warrantyVehicleRepairRate(@RequestBody DisplayRequestParam time) {
+        List<DisplayEntity> list = qualityDisplayService.selectWarrantyVehicleRepairRate(time.getStartTime(), time.getEndTime());
+        return getDataTable(list);
+    }
+
+    /**
+     * 外部质量损失率 指标59
+     */
+    @PostMapping("/externalLossRate")
+    public TableDataInfo externalLossRate(@RequestBody DisplayRequestParam time) {
+        List<DisplayEntity> list = qualityDisplayService.selectExternalLossRate(time.getStartTime(), time.getEndTime());
+        return getDataTable(list);
+    }
+
+    /**
+     * 月度售后质量问题总数  指标61
+     */
+    @PostMapping("/monthlyAfterSalesIssues")
+    public TableDataInfo monthlyAfterSalesIssues(@RequestBody DisplayRequestParam time) {
+        List<DisplayEntity> list = qualityDisplayService.selectMonthlyAfterSalesIssues(time.getStartTime(), time.getEndTime());
+        return getDataTable(list);
+    }
+
+    /**
+     * 售后问题生产责任次数 指标62
+     */
+    @PostMapping("/productionLiabilityIssues")
+    public TableDataInfo productionLiabilityIssues(@RequestBody DisplayRequestParam time) {
+        List<DisplayEntity> list = qualityDisplayService.selectProductionLiabilityIssues(time.getStartTime(), time.getEndTime());
+        return getDataTable(list);
+    }
+
+    /**
      * 质量考核季度排名 指标63
      */
     @PostMapping("/quarterlyRank")
@@ -46,6 +91,15 @@ public class QualityDisplayController extends BaseController {
     }
 
     /**
+     * 电车、大吨位一次交检合格率
+     */
+    @PostMapping("/singleInspectionPassRate")
+    public TableDataInfo singleInspectionPassRate(@RequestBody DisplayRequestParam time) {
+        List<DisplayEntity> list = qualityDisplayService.selectSingleInspectionPassRate(time.getStartTime(), time.getEndTime());
+        return getDataTable(list);
+    }
+
+    /**
      * 供应商不合格件返厂及时率
      */
     @PostMapping("/inTimeReturnRate")
@@ -53,6 +107,7 @@ public class QualityDisplayController extends BaseController {
         List<DisplayEntity> list = qualityDisplayService.selectInTimeReturnRate(time.getStartTime(), time.getEndTime());
         return getDataTable(list);
     }
+
 
     /**
      * 班组自查合格率 下道工序反馈合格率
--- digitalsystem-quality/src/main/java/com/heli/quality/mapper/QualityDisplayMapper.java
+++ digitalsystem-quality/src/main/java/com/heli/quality/mapper/QualityDisplayMapper.java
@@ -1,21 +1,43 @@
 package com.heli.quality.mapper;
 
 import com.ruoyi.common.core.domain.DisplayEntity;
+import org.apache.ibatis.annotations.MapKey;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 public interface QualityDisplayMapper {
 
-    //    质量考核季度排名 指标63
+    //三包期内新车返修率 指标57
+    List<DisplayEntity> selectWarrantyRepairRate(@Param("startTime") Date startTime, @Param("endTime") Date endTime);
+
+    //三包期内整车月度返修率 指标58
+    List<DisplayEntity> selectWarrantyVehicleRepairRate(@Param("startTime") Date startTime, @Param("endTime") Date endTime);
+
+    //外部质量损失率 指标59
+    List<DisplayEntity> selectExternalLossRate(@Param("startTime") Date startTime, @Param("endTime") Date endTime);
+
+    //月度售后质量问题总数  指标61
+    List<DisplayEntity> selectMonthlyAfterSalesIssues(@Param("startTime") Date startTime, @Param("endTime") Date endTime);
+
+    //售后问题生产责任次数 指标62
+    List<DisplayEntity> selectProductionLiabilityIssues(@Param("startTime") Date startTime, @Param("endTime") Date endTime);
+
+    //质量考核季度排名 指标63
     List<DisplayEntity> selectQuarterlyRank(@Param("startTime") Date startTime, @Param("endTime") Date endTime);
 
-    //    平均无故障时间 指标64
+    // 平均无故障时间 指标64
     List<DisplayEntity> selectMeantimeWithoutFailure(@Param("startTime") Date startTime, @Param("endTime") Date endTime);
 
+    //电车、大吨位一次交检合格率
+    List<DisplayEntity> selectSingleInspectionPassRate(@Param("startTime") Date startTime, @Param("endTime") Date endTime);
+
+    //供应商不合格件返厂及时率
     List<DisplayEntity> selectInTimeReturnRate(@Param("startTime") Date startTime, @Param("endTime") Date endTime);
 
+    //班组自查合格率、下道工序反馈合格率
     List<DisplayEntity> selectPartQualificationRate(@Param("startTime") Date startTime, @Param("endTime") Date endTime);
 
 

20843d825af6e4b272e5ca85bbdf8a54b63e2bc7
loophong <1940155085@qq.com>
2024-05-31 16:59:53 +0800

    党建权限

--- digitalsystem-partybuilding/src/main/java/com/heli/partybuilding/domain/PartyBuilding.java
+++ digitalsystem-partybuilding/src/main/java/com/heli/partybuilding/domain/PartyBuilding.java
@@ -3,6 +3,7 @@ package com.heli.partybuilding.domain;
 import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 import com.ruoyi.common.annotation.Excel;
@@ -13,6 +14,7 @@ import com.ruoyi.common.core.domain.BaseEntity;
  * @author: hong
  * @date: 2024/4/9 15:47
  **/
+@Data
 public class PartyBuilding extends BaseEntity {
     private static final long serialVersionUID = 1L;
 
@@ -32,7 +34,7 @@ public class PartyBuilding extends BaseEntity {
      * 党建排名
      */
     @Excel(name = "党建排名")
-    private Integer Ranking;
+    private Integer ranking;
 
     /**
      * 党建分数
@@ -40,107 +42,4 @@ public class PartyBuilding extends BaseEntity {
     @Excel(name = "党建分数")
     private Double score;
 
-    /**
-     * 创建人
-     */
-    @Excel(name = "创建人")
-    private String createdBy;
-
-    /**
-     * 创建时间
-     */
-    @JsonFormat(pattern = "yyyy-MM-dd")
-    @Excel(name = "创建时间", width = 30, dateFormat = "yyyy-MM-dd")
-    private Date createdTime;
-
-    /**
-     * 更新人
-     */
-    @Excel(name = "更新人")
-    private String updatedBy;
-
-    /**
-     * 更新时间
-     */
-    @JsonFormat(pattern = "yyyy-MM-dd")
-    @Excel(name = "更新时间", width = 30, dateFormat = "yyyy-MM-dd")
-    private Date updatedTime;
-
-    public void setPbId(String pbId) {
-        this.pbId = pbId;
-    }
-
-    public String getPbId() {
-        return pbId;
-    }
-
-    public void setYearAndMonth(Date yearAndMonth) {
-        this.yearAndMonth = yearAndMonth;
-    }
-
-    public Date getYearAndMonth() {
-        return yearAndMonth;
-    }
-
-    public void setRanking(Integer Ranking) {
-        this.Ranking = Ranking;
-    }
-
-    public Integer getRanking() {
-        return Ranking;
-    }
-
-    public void setScore(Double score) {
-        this.score = score;
-    }
-
-    public Double getScore() {
-        return score;
-    }
-
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    public String getCreatedBy() {
-        return createdBy;
-    }
-
-    public void setCreatedTime(Date createdTime) {
-        this.createdTime = createdTime;
-    }
-
-    public Date getCreatedTime() {
-        return createdTime;
-    }
-
-    public void setUpdatedBy(String updatedBy) {
-        this.updatedBy = updatedBy;
-    }
-
-    public String getUpdatedBy() {
-        return updatedBy;
-    }
-
-    public void setUpdatedTime(Date updatedTime) {
-        this.updatedTime = updatedTime;
-    }
-
-    public Date getUpdatedTime() {
-        return updatedTime;
-    }
-
-    @Override
-    public String toString() {
-        return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE)
-                .append("pbId", getPbId())
-                .append("yearAndMonth", getYearAndMonth())
-                .append("Ranking", getRanking())
-                .append("score", getScore())
-                .append("createdBy", getCreatedBy())
-                .append("createdTime", getCreatedTime())
-                .append("updatedBy", getUpdatedBy())
-                .append("updatedTime", getUpdatedTime())
-                .toString();
-    }
 }

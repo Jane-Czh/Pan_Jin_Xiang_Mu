b7bd59a24ae7da167f393b29a5dfe33ac380ebe2
loophong <1940155085@qq.com>
2024-09-11 15:47:22 +0800

    9-11 指标-技术-非标订单

--- digitalsystem-tech/src/main/java/com/heli/tech/service/impl/TechNonStandardOrderServiceImpl.java
+++ digitalsystem-tech/src/main/java/com/heli/tech/service/impl/TechNonStandardOrderServiceImpl.java
@@ -71,7 +71,7 @@ public class TechNonStandardOrderServiceImpl implements ITechNonStandardOrderSer
             tech.setNonStandardOvertimeNum(nonStandardOrderOvertimeNum);
             techService.insertOrUpdateTech(tech);
 
-            log.info("统计结果：超时单数："+nonStandardOrderOvertimeNum+"，超时单平均时长："+nonStandardOrderAvgDays);
+            log.info("统计结果：超时单数："+nonStandardOrderOvertimeNum+"，平均时长："+nonStandardOrderAvgDays);
 
             return R.ok("读取" + fileName + "文件成功");
         } catch (Exception e) {

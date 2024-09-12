eba01e2c6505e413dfa8d3b2bd748fb9ba1377ba
loophong <1940155085@qq.com>
2024-09-12 17:50:36 +0800

    9-12 指标-企管-日清日结增加分数

--- digitalsystem-enterprise/src/main/java/com/heli/enterprise/controller/EnterpriseManagementIndicatorsDailyClearingSettlementController.java
+++ digitalsystem-enterprise/src/main/java/com/heli/enterprise/controller/EnterpriseManagementIndicatorsDailyClearingSettlementController.java
@@ -1,7 +1,6 @@
 package com.heli.enterprise.controller;
 
 import java.util.List;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -24,9 +23,11 @@ import com.ruoyi.common.utils.poi.ExcelUtil;
 import com.ruoyi.common.core.page.TableDataInfo;
 
 /**
- * 日清日结 Controller
+ * 日清日结
+ * Controller
+ *
  * @author hong
- * @date 2024-05-15
+ * @date 2024-09-12
  */
 @RestController
 @RequestMapping("/enterprise/data/dailyclear")
@@ -35,7 +36,8 @@ public class EnterpriseManagementIndicatorsDailyClearingSettlementController ext
     private IEnterpriseManagementIndicatorsDailyClearingSettlementService enterpriseManagementIndicatorsDailyClearingSettlementService;
 
     /**
-     * 查询日清日结列表
+     * 查询日清日结
+     * 列表
      */
     @PreAuthorize("@ss.hasPermi('enterprise:dailyclear:list')")
     @GetMapping("/list")
@@ -46,11 +48,25 @@ public class EnterpriseManagementIndicatorsDailyClearingSettlementController ext
     }
 
     /**
-     * 获取日清日结详细信息
+     * 导出日清日结
+     * 列表
+     */
+//    @PreAuthorize("@ss.hasPermi('enterprise:dailyclear:export')")
+//    @Log(title = "日清日结 ", businessType = BusinessType.EXPORT)
+//    @PostMapping("/export")
+//    public void export(HttpServletResponse response, EnterpriseManagementIndicatorsDailyClearingSettlement enterpriseManagementIndicatorsDailyClearingSettlement) {
+//        List<EnterpriseManagementIndicatorsDailyClearingSettlement> list = enterpriseManagementIndicatorsDailyClearingSettlementService.selectEnterpriseManagementIndicatorsDailyClearingSettlementList(enterpriseManagementIndicatorsDailyClearingSettlement);
+//        ExcelUtil<EnterpriseManagementIndicatorsDailyClearingSettlement> util = new ExcelUtil<EnterpriseManagementIndicatorsDailyClearingSettlement>(EnterpriseManagementIndicatorsDailyClearingSettlement.class);
+//        util.exportExcel(response, list, "日清日结数据");
+//    }
+
+    /**
+     * 获取日清日结
+     * 详细信息
      */
     @PreAuthorize("@ss.hasPermi('enterprise:dailyclear:query')")
     @GetMapping(value = "/{edId}")
-    public AjaxResult getInfo(@PathVariable("edId") Long edId) {
+    public AjaxResult getInfo(@PathVariable("edId") String edId) {
         return success(enterpriseManagementIndicatorsDailyClearingSettlementService.selectEnterpriseManagementIndicatorsDailyClearingSettlementByEdId(edId));
     }
 
@@ -62,12 +78,16 @@ public class EnterpriseManagementIndicatorsDailyClearingSettlementController ext
     @PostMapping
     public AjaxResult add(@RequestBody EnterpriseManagementIndicatorsDailyClearingSettlement enterpriseManagementIndicatorsDailyClearingSettlement) {
         System.out.println('1');
-        if (enterpriseManagementIndicatorsDailyClearingSettlementService.checkDailyClearingDataIsExisted(enterpriseManagementIndicatorsDailyClearingSettlement.getYearAndMonth())){
+        if (enterpriseManagementIndicatorsDailyClearingSettlementService.checkDailyClearingDataIsExisted(enterpriseManagementIndicatorsDailyClearingSettlement.getYearAndMonth())) {
             return AjaxResult.warn("当月数据已上传");
         }
         enterpriseManagementIndicatorsDailyClearingSettlement.setCreateBy(getUsername());
         return toAjax(enterpriseManagementIndicatorsDailyClearingSettlementService.insertEnterpriseManagementIndicatorsDailyClearingSettlement(enterpriseManagementIndicatorsDailyClearingSettlement));
     }
+//    public AjaxResult add(@RequestBody EnterpriseManagementIndicatorsDailyClearingSettlement enterpriseManagementIndicatorsDailyClearingSettlement) {
+//        return toAjax(enterpriseManagementIndicatorsDailyClearingSettlementService.insertEnterpriseManagementIndicatorsDailyClearingSettlement(enterpriseManagementIndicatorsDailyClearingSettlement));
+//    }
+
 
     /**
      * 修改日清日结
@@ -76,7 +96,6 @@ public class EnterpriseManagementIndicatorsDailyClearingSettlementController ext
 //    @Log(title = "日清日结 ", businessType = BusinessType.UPDATE)
     @PutMapping
     public AjaxResult edit(@RequestBody EnterpriseManagementIndicatorsDailyClearingSettlement enterpriseManagementIndicatorsDailyClearingSettlement) {
-        enterpriseManagementIndicatorsDailyClearingSettlement.setUpdateBy(getUsername());
         return toAjax(enterpriseManagementIndicatorsDailyClearingSettlementService.updateEnterpriseManagementIndicatorsDailyClearingSettlement(enterpriseManagementIndicatorsDailyClearingSettlement));
     }
 
@@ -84,9 +103,103 @@ public class EnterpriseManagementIndicatorsDailyClearingSettlementController ext
      * 删除日清日结
      */
     @PreAuthorize("@ss.hasPermi('enterprise:dailyclear:remove')")
-//    @Log(title = "日清日结 ", businessType = BusinessType.DELETE)
+    @Log(title = "日清日结 ", businessType = BusinessType.DELETE)
     @DeleteMapping("/{edIds}")
-    public AjaxResult remove(@PathVariable Long[] edIds) {
+    public AjaxResult remove(@PathVariable String[] edIds) {
         return toAjax(enterpriseManagementIndicatorsDailyClearingSettlementService.deleteEnterpriseManagementIndicatorsDailyClearingSettlementByEdIds(edIds));
     }
 }
+
+
+//package com.heli.enterprise.controller;
+//
+//import java.util.List;
+//import javax.servlet.ServletOutputStream;
+//import javax.servlet.http.HttpServletResponse;
+//
+//import org.springframework.security.access.prepost.PreAuthorize;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.PutMapping;
+//import org.springframework.web.bind.annotation.DeleteMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//import com.ruoyi.common.annotation.Log;
+//import com.ruoyi.common.core.controller.BaseController;
+//import com.ruoyi.common.core.domain.AjaxResult;
+//import com.ruoyi.common.enums.BusinessType;
+//import com.heli.enterprise.domain.EnterpriseManagementIndicatorsDailyClearingSettlement;
+//import com.heli.enterprise.service.IEnterpriseManagementIndicatorsDailyClearingSettlementService;
+//import com.ruoyi.common.utils.poi.ExcelUtil;
+//import com.ruoyi.common.core.page.TableDataInfo;
+//
+///**
+// * 日清日结 Controller
+// * @author hong
+// * @date 2024-05-15
+// */
+//@RestController
+//@RequestMapping("/enterprise/data/dailyclear")
+//public class EnterpriseManagementIndicatorsDailyClearingSettlementController extends BaseController {
+//    @Autowired
+//    private IEnterpriseManagementIndicatorsDailyClearingSettlementService enterpriseManagementIndicatorsDailyClearingSettlementService;
+//
+//    /**
+//     * 查询日清日结列表
+//     */
+//    @PreAuthorize("@ss.hasPermi('enterprise:dailyclear:list')")
+//    @GetMapping("/list")
+//    public TableDataInfo list(EnterpriseManagementIndicatorsDailyClearingSettlement enterpriseManagementIndicatorsDailyClearingSettlement) {
+//        startPage();
+//        List<EnterpriseManagementIndicatorsDailyClearingSettlement> list = enterpriseManagementIndicatorsDailyClearingSettlementService.selectEnterpriseManagementIndicatorsDailyClearingSettlementList(enterpriseManagementIndicatorsDailyClearingSettlement);
+//        return getDataTable(list);
+//    }
+//
+//    /**
+//     * 获取日清日结详细信息
+//     */
+//    @PreAuthorize("@ss.hasPermi('enterprise:dailyclear:query')")
+//    @GetMapping(value = "/{edId}")
+//    public AjaxResult getInfo(@PathVariable("edId") Long edId) {
+//        return success(enterpriseManagementIndicatorsDailyClearingSettlementService.selectEnterpriseManagementIndicatorsDailyClearingSettlementByEdId(edId));
+//    }
+//
+//    /**
+//     * 新增日清日结
+//     */
+//    @PreAuthorize("@ss.hasPermi('enterprise:dailyclear:add')")
+//    @Log(title = "日清日结 ", businessType = BusinessType.INSERT)
+//    @PostMapping
+//    public AjaxResult add(@RequestBody EnterpriseManagementIndicatorsDailyClearingSettlement enterpriseManagementIndicatorsDailyClearingSettlement) {
+//        System.out.println('1');
+//        if (enterpriseManagementIndicatorsDailyClearingSettlementService.checkDailyClearingDataIsExisted(enterpriseManagementIndicatorsDailyClearingSettlement.getYearAndMonth())){
+//            return AjaxResult.warn("当月数据已上传");
+//        }
+//        enterpriseManagementIndicatorsDailyClearingSettlement.setCreateBy(getUsername());
+//        return toAjax(enterpriseManagementIndicatorsDailyClearingSettlementService.insertEnterpriseManagementIndicatorsDailyClearingSettlement(enterpriseManagementIndicatorsDailyClearingSettlement));
+//    }
+//
+//    /**
+//     * 修改日清日结
+//     */
+//    @PreAuthorize("@ss.hasPermi('enterprise:dailyclear:edit')")
+////    @Log(title = "日清日结 ", businessType = BusinessType.UPDATE)
+//    @PutMapping
+//    public AjaxResult edit(@RequestBody EnterpriseManagementIndicatorsDailyClearingSettlement enterpriseManagementIndicatorsDailyClearingSettlement) {
+//        enterpriseManagementIndicatorsDailyClearingSettlement.setUpdateBy(getUsername());
+//        return toAjax(enterpriseManagementIndicatorsDailyClearingSettlementService.updateEnterpriseManagementIndicatorsDailyClearingSettlement(enterpriseManagementIndicatorsDailyClearingSettlement));
+//    }
+//
+//    /**
+//     * 删除日清日结
+//     */
+//    @PreAuthorize("@ss.hasPermi('enterprise:dailyclear:remove')")
+////    @Log(title = "日清日结 ", businessType = BusinessType.DELETE)
+//    @DeleteMapping("/{edIds}")
+//    public AjaxResult remove(@PathVariable Long[] edIds) {
+//        return toAjax(enterpriseManagementIndicatorsDailyClearingSettlementService.deleteEnterpriseManagementIndicatorsDailyClearingSettlementByEdIds(edIds));
+//    }
+//}
--- digitalsystem-enterprise/src/main/java/com/heli/enterprise/service/impl/EnterpriseManagementIndicatorsDailyClearingSettlementServiceImpl.java
+++ digitalsystem-enterprise/src/main/java/com/heli/enterprise/service/impl/EnterpriseManagementIndicatorsDailyClearingSettlementServiceImpl.java
@@ -15,12 +15,13 @@ import com.heli.enterprise.service.IEnterpriseManagementIndicatorsDailyClearingS
  * Service业务层处理
  *
  * @author hong
- * @date 2024-05-15
+ * @date 2024-09-12
  */
 @Service
 public class EnterpriseManagementIndicatorsDailyClearingSettlementServiceImpl implements IEnterpriseManagementIndicatorsDailyClearingSettlementService {
     @Autowired
     private EnterpriseManagementIndicatorsDailyClearingSettlementMapper enterpriseManagementIndicatorsDailyClearingSettlementMapper;
+
 
     @Override
     public Boolean checkDailyClearingDataIsExisted(Date yearAndMonth) {
@@ -29,13 +30,11 @@ public class EnterpriseManagementIndicatorsDailyClearingSettlementServiceImpl im
 
     /**
      * 查询日清日结
-     *
-     * @param edId 日清日结
-     *             主键
+     * @param edId 日清日结 主键
      * @return 日清日结
      */
     @Override
-    public EnterpriseManagementIndicatorsDailyClearingSettlement selectEnterpriseManagementIndicatorsDailyClearingSettlementByEdId(Long edId) {
+    public EnterpriseManagementIndicatorsDailyClearingSettlement selectEnterpriseManagementIndicatorsDailyClearingSettlementByEdId(String edId) {
         return enterpriseManagementIndicatorsDailyClearingSettlementMapper.selectEnterpriseManagementIndicatorsDailyClearingSettlementByEdId(edId);
     }
 
@@ -83,7 +82,7 @@ public class EnterpriseManagementIndicatorsDailyClearingSettlementServiceImpl im
      * @return 结果
      */
     @Override
-    public int deleteEnterpriseManagementIndicatorsDailyClearingSettlementByEdIds(Long[] edIds) {
+    public int deleteEnterpriseManagementIndicatorsDailyClearingSettlementByEdIds(String[] edIds) {
         return enterpriseManagementIndicatorsDailyClearingSettlementMapper.deleteEnterpriseManagementIndicatorsDailyClearingSettlementByEdIds(edIds);
     }
 
@@ -96,7 +95,111 @@ public class EnterpriseManagementIndicatorsDailyClearingSettlementServiceImpl im
      * @return 结果
      */
     @Override
-    public int deleteEnterpriseManagementIndicatorsDailyClearingSettlementByEdId(Long edId) {
+    public int deleteEnterpriseManagementIndicatorsDailyClearingSettlementByEdId(String edId) {
         return enterpriseManagementIndicatorsDailyClearingSettlementMapper.deleteEnterpriseManagementIndicatorsDailyClearingSettlementByEdId(edId);
     }
 }
+
+
+//package com.heli.enterprise.service.impl;
+//
+//import java.util.Date;
+//import java.util.List;
+//
+//import com.ruoyi.common.utils.DateUtils;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//import com.heli.enterprise.mapper.EnterpriseManagementIndicatorsDailyClearingSettlementMapper;
+//import com.heli.enterprise.domain.EnterpriseManagementIndicatorsDailyClearingSettlement;
+//import com.heli.enterprise.service.IEnterpriseManagementIndicatorsDailyClearingSettlementService;
+//
+///**
+// * 日清日结
+// * Service业务层处理
+// *
+// * @author hong
+// * @date 2024-05-15
+// */
+//@Service
+//public class EnterpriseManagementIndicatorsDailyClearingSettlementServiceImpl implements IEnterpriseManagementIndicatorsDailyClearingSettlementService {
+//    @Autowired
+//    private EnterpriseManagementIndicatorsDailyClearingSettlementMapper enterpriseManagementIndicatorsDailyClearingSettlementMapper;
+//
+//    @Override
+//    public Boolean checkDailyClearingDataIsExisted(Date yearAndMonth) {
+//        return enterpriseManagementIndicatorsDailyClearingSettlementMapper.checkDailyClearingDataIsExisted(yearAndMonth);
+//    }
+//
+//    /**
+//     * 查询日清日结
+//     *
+//     * @param edId 日清日结
+//     *             主键
+//     * @return 日清日结
+//     */
+//    @Override
+//    public EnterpriseManagementIndicatorsDailyClearingSettlement selectEnterpriseManagementIndicatorsDailyClearingSettlementByEdId(Long edId) {
+//        return enterpriseManagementIndicatorsDailyClearingSettlementMapper.selectEnterpriseManagementIndicatorsDailyClearingSettlementByEdId(edId);
+//    }
+//
+//    /**
+//     * 查询日清日结
+//     * 列表
+//     *
+//     * @param enterpriseManagementIndicatorsDailyClearingSettlement 日清日结
+//     * @return 日清日结
+//     */
+//    @Override
+//    public List<EnterpriseManagementIndicatorsDailyClearingSettlement> selectEnterpriseManagementIndicatorsDailyClearingSettlementList(EnterpriseManagementIndicatorsDailyClearingSettlement enterpriseManagementIndicatorsDailyClearingSettlement) {
+//        return enterpriseManagementIndicatorsDailyClearingSettlementMapper.selectEnterpriseManagementIndicatorsDailyClearingSettlementList(enterpriseManagementIndicatorsDailyClearingSettlement);
+//    }
+//
+//    /**
+//     * 新增日清日结
+//     *
+//     * @param enterpriseManagementIndicatorsDailyClearingSettlement 日清日结
+//     * @return 结果
+//     */
+//    @Override
+//    public int insertEnterpriseManagementIndicatorsDailyClearingSettlement(EnterpriseManagementIndicatorsDailyClearingSettlement enterpriseManagementIndicatorsDailyClearingSettlement) {
+//        enterpriseManagementIndicatorsDailyClearingSettlement.setCreateTime(DateUtils.getNowDate());
+//        return enterpriseManagementIndicatorsDailyClearingSettlementMapper.insertEnterpriseManagementIndicatorsDailyClearingSettlement(enterpriseManagementIndicatorsDailyClearingSettlement);
+//    }
+//
+//    /**
+//     * 修改日清日结
+//     *
+//     * @param enterpriseManagementIndicatorsDailyClearingSettlement 日清日结
+//     * @return 结果
+//     */
+//    @Override
+//    public int updateEnterpriseManagementIndicatorsDailyClearingSettlement(EnterpriseManagementIndicatorsDailyClearingSettlement enterpriseManagementIndicatorsDailyClearingSettlement) {
+//        enterpriseManagementIndicatorsDailyClearingSettlement.setUpdateTime(DateUtils.getNowDate());
+//        return enterpriseManagementIndicatorsDailyClearingSettlementMapper.updateEnterpriseManagementIndicatorsDailyClearingSettlement(enterpriseManagementIndicatorsDailyClearingSettlement);
+//    }
+//
+//    /**
+//     * 批量删除日清日结
+//     *
+//     * @param edIds 需要删除的日清日结
+//     *              主键
+//     * @return 结果
+//     */
+//    @Override
+//    public int deleteEnterpriseManagementIndicatorsDailyClearingSettlementByEdIds(Long[] edIds) {
+//        return enterpriseManagementIndicatorsDailyClearingSettlementMapper.deleteEnterpriseManagementIndicatorsDailyClearingSettlementByEdIds(edIds);
+//    }
+//
+//    /**
+//     * 删除日清日结
+//     * 信息
+//     *
+//     * @param edId 日清日结
+//     *             主键
+//     * @return 结果
+//     */
+//    @Override
+//    public int deleteEnterpriseManagementIndicatorsDailyClearingSettlementByEdId(Long edId) {
+//        return enterpriseManagementIndicatorsDailyClearingSettlementMapper.deleteEnterpriseManagementIndicatorsDailyClearingSettlementByEdId(edId);
+//    }
+//}
